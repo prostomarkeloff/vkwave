390950e104943420f1c4883821a1d36dbb625d35
prostomarkeloff <28061158+prostomarkeloff@users.noreply.github.com>
2020-02-19 11:20:39 +0300

    add logging to vkwave-client

--- vkwave-client/vkwave_client/default.py
+++ vkwave-client/vkwave_client/default.py
@@ -9,6 +9,11 @@ from .abstract import AbstractHTTPClient, MethodName
 
 from typing import Optional
 
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
 
 class AIOHTTPClient(AbstractHTTPClient):
     def __init__(
@@ -20,10 +25,12 @@ class AIOHTTPClient(AbstractHTTPClient):
         self._session = session or ClientSession(loop=self._loop)
 
     async def request(self, method_name: MethodName, **kwargs) -> dict:
+        logger.debug(f"Doing request to '{method_name}' method with these params: {kwargs}")
         async with self._session.post(
             self.API_URL.format(method_name=method_name), data=kwargs
         ) as resp:
             return await resp.json()
 
     async def close(self) -> None:
+        logger.debug("Closing aiohttp session...")
         await self._session.close()
