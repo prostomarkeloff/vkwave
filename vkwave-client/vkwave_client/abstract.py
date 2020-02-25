78c9124521b3106913a265e42e2026a391e71bba
prostomarkeloff <28061158+prostomarkeloff@users.noreply.github.com>
2020-02-25 16:36:23 +0300

    Standard __aenter__, __aexit__ for client

--- vkwave-client/vkwave_client/abstract.py
+++ vkwave-client/vkwave_client/abstract.py
@@ -3,11 +3,16 @@ from abc import abstractmethod, ABC
 from .types import MethodName
 from .context import RequestContext
 
-
 class AbstractAPIClient(ABC):
     @abstractmethod
     def create_request(self, method_name: MethodName, **params) -> RequestContext:
         ...
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        await self.close()
 
     @abstractmethod
     async def close(self) -> None:
