73b8bc4dcc39cd8e0665f0e67dd00c4c73cac6b1
prostomarkeloff <28061158+prostomarkeloff@users.noreply.github.com>
2020-03-16 14:46:49 +0300

    add some imports to __init__

--- vkwave-bots/vkwave_bots/__init__.py
+++ vkwave-bots/vkwave_bots/__init__.py
@@ -1,1 +1,6 @@
+from .dispatching.filters.base import BaseFilter
+from .dispatching.filters.base_filters import EventTypeFilter
+from .dispatching.dp.dp import Dispatcher
+from .dispatching.extensions.base import BaseExtension
+
 __version__ = "0.1.0"
