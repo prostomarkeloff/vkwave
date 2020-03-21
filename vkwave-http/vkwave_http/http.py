22a8275d6c99f3bb899b6bed75ce8e8f6eb2e62d
prostomarkeloff <28061158+prostomarkeloff@users.noreply.github.com>
2020-03-21 16:07:36 +0300

    fix

--- vkwave-http/vkwave_http/http.py
+++ vkwave-http/vkwave_http/http.py
@@ -8,7 +8,11 @@ from aiohttp import ClientSession
 
 class AbstractHTTPClient(ABC):
     @abstractmethod
-    async def request(self, method: str, url: str, data: Optional[dict] = None) -> dict:
+    async def request_json(self, method: str, url: str, data: Optional[dict] = None) -> dict:
+        ...
+    
+    @abstractmethod
+    async def request_text(self, method: str, url: str, data: Optional[dict] = None) -> str:
         ...
 
     @abstractmethod
@@ -19,7 +23,7 @@ class AbstractHTTPClient(ABC):
 class AIOHTTPClient(AbstractHTTPClient):
     def __init__(self, session: Optional[ClientSession] = None, loop: Optional[AEL] = None):
         self.loop = loop or get_event_loop()
-        self.session = ClientSession(loop=self.loop)
+        self.session = session or ClientSession(loop=self.loop)
 
     async def close(self):
         await self.session.close()
