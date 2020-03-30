d76e3bd68ac2393787890775381138e817bdf3fb
tdakkota <tanc13@yandex.ru>
2020-03-30 22:34:09 +0300

    Add Redis bots storage (#6)
    * Add RedisStorage, Dumper and Loader typings
    
    * Some requested changes

--- vkwave-bots-storage/vkwave_bots_storage/types.py
+++ vkwave-bots-storage/vkwave_bots_storage/types.py
@@ -3,3 +3,6 @@ import typing
 Key = typing.NewType("Key", str)
 Value = typing.Any
 TTL = typing.NewType("TTL", float)
+
+Dumper = typing.Callable[[Value], str]
+Loader = typing.Callable[[str], Value]
