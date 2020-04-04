e76c28d6dc4f057a11e82753b027a6fcdd696557
tdakkota <tanc13@yandex.ru>
2020-04-04 19:15:09 +0300

    Some fixes in bots/utils/uploaders (#29)
    * Add uploader tests
    
    * Add from url uploader, some refactor
    
    * Some fixes
    
    * Reformat, fix flake8 warning
    
    * Move easy.py to bots package

---
+++ vkwave/bots/utils/uploaders/__init__.py
@@ -0,0 +1,3 @@
+from .doc_uploader import DocUploader, GraffitiUploader, VoiceUploader  # noqa: F401
+from .photo_uploader import PhotoUploader  # noqa: F401
+from .uploader import BaseUploader  # noqa: F401
--- vkwave/bots/utils/uploaders/photo_uploader.py
+++ vkwave/bots/utils/uploaders/photo_uploader.py
@@ -1,31 +1,30 @@
-import json
-import os
 import typing
 
-from vkwave.api.methods import APIOptionsRequestContext
-from vkwave.bots.core.types.json_types import JSONDecoder
-from vkwave.http import AbstractHTTPClient
+from vkwave.bots.utils.uploaders.uploader import BaseUploader
 from vkwave.types.objects import PhotosPhoto
 
 
-class PhotoUploader:
-    def __init__(
-        self, api_context: APIOptionsRequestContext, json_serialize: JSONDecoder = json.loads,
-    ):
-        self.api_context = api_context
-        self.client: AbstractHTTPClient = api_context.api_options.get_client().http_client
-        self.json_serialize = json_serialize
-
+class PhotoUploader(BaseUploader[typing.List[PhotosPhoto]]):
     async def get_server(self, peer_id: int) -> str:
         server_data = await self.api_context.photos.get_messages_upload_server(peer_id=peer_id)
         return server_data.response.upload_url
 
-    async def upload(self, upload_url: str, file_data) -> typing.List[PhotosPhoto]:
-        upload_data = self.json_serialize(
+    async def upload(
+        self, upload_url: str, file_data: typing.BinaryIO
+    ) -> typing.List[PhotosPhoto]:
+        # really dirty hack
+        # but it works
+        if not hasattr(file_data, "name"):
+            setattr(file_data, "name", "file.jpg")
+
+        upload_data = self.json_deserialize(
             await self.client.request_text(
-                method="POST", url=upload_url, data={"photo": file_data}
+                method="POST", url=upload_url, data={"file1": file_data}
             )
         )
+
+        self.handle_error(upload_data)
+
         photo_sizes = (
             await self.api_context.photos.save_messages_photo(
                 photo=upload_data["photo"], server=upload_data["server"], hash=upload_data["hash"],
@@ -33,32 +32,10 @@ class PhotoUploader:
         ).response
         return photo_sizes
 
-    async def get_attachment_from_path(self, peer_id: int, file_path: str) -> str:
-        upload_url = await self.get_server(peer_id)
-        file_data = open(file_path, "rb")
-        photo = await self.upload(upload_url, file_data)
-        file_data.close()
-        return f"photo{photo[-1].owner_id}_{photo[-1].id}"
-
-    async def get_attachments_from_paths(self, peer_id: int, file_paths: typing.List[str]) -> str:
-        ready_attachments: typing.List[str] = []
-        for file in file_paths:
-            ready_attachments.append(await self.get_attachment_from_path(peer_id, file))
-        return ",".join(ready_attachments)
-
-    async def get_attachment_from_link(self, peer_id: int, link: str) -> str:
-        filename = "__vkwave__temp__file__.jpg"
-        photo_data = await self.client.request_data(method="GET", url=link)
-
-        #  TODO: async saving
-        with open(filename, "wb") as file:
-            file.write(photo_data)
-        attachment = await self.get_attachment_from_path(file_path=filename, peer_id=peer_id)
-        os.remove(filename)
-        return attachment
-
-    async def get_attachments_from_links(self, peer_id: int, links: typing.List[str]) -> str:
-        ready_attachments: typing.List[str] = []
-        for link in links:
-            ready_attachments.append(await self.get_attachment_from_link(peer_id, link))
-        return ",".join(ready_attachments)
+    def attachment_name(self, photo: typing.List[PhotosPhoto]) -> str:
+        p = photo[-1]
+        return (
+            f"photo{p.owner_id}_{p.id}"
+            if not p.access_key
+            else f"photo{p.owner_id}_{p.id}_{p.access_key}"
+        )
