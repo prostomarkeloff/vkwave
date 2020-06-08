eb1378f03b2c8a574243124350dfe8fa330e0160
kesha1225 <samedov03@mail.ru>
2020-06-08 12:42:26 +0300

    event type check

--- examples/bots/low_level_lp.py
+++ examples/bots/low_level_lp.py
@@ -1,65 +1,74 @@
 from vkwave.bots import LowLevelBot
+from vkwave.types.bot_events import BotEventType
+
 
 bot = LowLevelBot("Token", 12345)  # Bot
 
-#  bot = LowLevelBot("Token") # User
+#  bot = LowLevelBot("Token") # User (not recommended, because events will come in strange lists)
 
 
 async def not_fast_mode():
     # You can ignore all lp errors
     async for event in bot.listen(ignore_errors=True):
-        if event.object.message.text == "/start":
-            bot.react(
-                bot.api_context.messages.send(
-                    peer_id=event.object.message.peer_id,
-                    message="i got start",
-                    random_id=0,
+
+        # for User:
+        # from vkwave.types.user_events import EventId
+        # event[0] == EventId.MESSAGE_EVENT
+
+        if event.type == BotEventType.MESSAGE_NEW:
+            if event.object.message.text == "/start":
+                bot.react(
+                    bot.api_context.messages.send(
+                        peer_id=event.object.message.peer_id,
+                        message="i got start",
+                        random_id=0,
+                    )
                 )
-            )
-        elif event.object.message.text == "/end":
-            bot.react(
-                bot.api_context.messages.send(
-                    peer_id=event.object.message.peer_id,
-                    message="i got end",
-                    random_id=0,
+            elif event.object.message.text == "/end":
+                bot.react(
+                    bot.api_context.messages.send(
+                        peer_id=event.object.message.peer_id,
+                        message="i got end",
+                        random_id=0,
+                    )
                 )
-            )
-        else:
-            bot.react(
-                bot.api_context.messages.send(
-                    peer_id=event.object.message.peer_id,
-                    message="don't understand",
-                    random_id=0,
+            else:
+                bot.react(
+                    bot.api_context.messages.send(
+                        peer_id=event.object.message.peer_id,
+                        message="don't understand",
+                        random_id=0,
+                    )
                 )
-            )
 
 
 async def fast_mode():
     async for event in bot.listen(fast_mode=True):
-        if event["object"]["message"]["text"] == "/start":
-            bot.react(
-                bot.api_context.messages.send(
-                    peer_id=event["object"]["message"]["peer_id"],
-                    message="i got start",
-                    random_id=0,
+        if event["type"] == BotEventType.MESSAGE_NEW:
+            if event["object"]["message"]["text"] == "/start":
+                bot.react(
+                    bot.api_context.messages.send(
+                        peer_id=event["object"]["message"]["peer_id"],
+                        message="i got start",
+                        random_id=0,
+                    )
                 )
-            )
-        elif event["object"]["message"]["text"] == "/end":
-            bot.react(
-                bot.api_context.messages.send(
-                    peer_id=event["object"]["message"]["peer_id"],
-                    message="i got end",
-                    random_id=0,
+            elif event["object"]["message"]["text"] == "/end":
+                bot.react(
+                    bot.api_context.messages.send(
+                        peer_id=event["object"]["message"]["peer_id"],
+                        message="i got end",
+                        random_id=0,
+                    )
                 )
-            )
-        else:
-            bot.react(
-                bot.api_context.messages.send(
-                    peer_id=event["object"]["message"]["peer_id"],
-                    message="don't understand",
-                    random_id=0,
+            else:
+                bot.react(
+                    bot.api_context.messages.send(
+                        peer_id=event["object"]["message"]["peer_id"],
+                        message="don't understand",
+                        random_id=0,
+                    )
                 )
-            )
 
 
 # bot.run(main_loop=fast_mode())
