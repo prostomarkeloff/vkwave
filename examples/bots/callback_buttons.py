a1f27b7615b417911bd00d1937a8a092697c605f
kesha1225 <samedov03@mail.ru>
2020-07-14 18:32:26 +0300

    callback buttons example

---
+++ examples/bots/callback_buttons.py
@@ -0,0 +1,19 @@
+from vkwave.bots import SimpleLongPollBot, Keyboard, CallbackAnswer
+from vkwave.types.bot_events import BotEventType
+
+bot = SimpleLongPollBot(tokens=["Token"], group_id=123)
+
+
+@bot.message_handler(bot.text_filter("123"))
+async def simple(event: bot.SimpleBotEvent):
+    kb = Keyboard()
+    kb.add_callback_button(payload={"21321": 123}, text="123")
+    await event.answer("HELLO", keyboard=kb.get_keyboard())
+
+
+@bot.handler(bot.event_type_filter(BotEventType.MESSAGE_EVENT))
+async def all(event: bot.SimpleBotEvent):
+    await event.callback_answer(event_data=CallbackAnswer.open_link(link="https://google.com"))
+
+
+bot.run_forever()
