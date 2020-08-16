17f6c99a4a323e386c18d55c497ec59b0c1e1a7e
kesha1225 <samedov03@mail.ru>
2020-08-17 01:58:47 +0300

    tags to doc uploader

--- vkwave/bots/utils/uploaders/doc_uploader.py
+++ vkwave/bots/utils/uploaders/doc_uploader.py
@@ -16,7 +16,11 @@ class DocUploaderMixin(BaseUploader[DocsSaveResponseModel], ABC):
         return server_data.response.upload_url
 
     async def upload(
-        self, upload_url: str, file_data: BinaryIO, title: typing.Optional[str] = None
+        self,
+        upload_url: str,
+        file_data: BinaryIO,
+        title: typing.Optional[str] = None,
+        tags: typing.Optional[str] = None,
     ) -> DocsSaveResponseModel:
         # really dirty hack
         # but it works
@@ -33,45 +37,69 @@ class DocUploaderMixin(BaseUploader[DocsSaveResponseModel], ABC):
         self.handle_error(upload_data)
 
         doc_save = (
-            await self.api_context.docs.save(file=upload_data["file"], title=title)
+            await self.api_context.docs.save(file=upload_data["file"], title=title, tags=tags)
         ).response
         return doc_save
 
     async def get_attachment_from_io(
-        self, peer_id: int, f: BinaryIO, title: typing.Optional[str] = None
+        self,
+        peer_id: int,
+        f: BinaryIO,
+        title: typing.Optional[str] = None,
+        tags: typing.Optional[str] = None,
     ) -> str:
         upload_url = await self.get_server(peer_id)
-        return self.attachment_name(await self.upload(upload_url, f, title=title))
+        return self.attachment_name(await self.upload(upload_url, f, title=title, tags=tags))
 
     async def get_attachment_from_path(
-        self, peer_id: int, file_path: str, title: typing.Optional[str] = None
+        self,
+        peer_id: int,
+        file_path: str,
+        title: typing.Optional[str] = None,
+        tags: typing.Optional[str] = None,
     ) -> str:
         with open(file_path, "rb") as file_data:
-            return await self.get_attachment_from_io(peer_id, file_data, title=title)
+            return await self.get_attachment_from_io(peer_id, file_data, title=title, tags=tags)
 
     async def get_attachments_from_paths(
-        self, peer_id: int, file_paths: typing.List[str], titles: typing.Sequence[str] = ()
+        self,
+        peer_id: int,
+        file_paths: typing.List[str],
+        titles: typing.Sequence[str] = (),
+        tags_list: typing.Sequence[str] = (),
     ) -> str:
         ready_attachments: typing.List[str] = []
-        for file, title in itertools.zip_longest(file_paths, titles, fillvalue="Document"):
+        for file, title, tags in itertools.zip_longest(
+            file_paths, titles, tags_list, fillvalue="Document"
+        ):
             ready_attachments.append(
-                await self.get_attachment_from_path(peer_id, file, title=title)
+                await self.get_attachment_from_path(peer_id, file, title=title, tags=tags)
             )
         return ",".join(ready_attachments)
 
     async def get_attachment_from_link(
-        self, peer_id: int, link: str, title: typing.Optional[str] = None
+        self,
+        peer_id: int,
+        link: str,
+        title: typing.Optional[str] = None,
+        tags: typing.Optional[str] = None,
     ) -> str:
         file_data = BytesIO(await self.client.request_data("GET", link))
-        return await self.get_attachment_from_io(peer_id, file_data, title=title)
+        return await self.get_attachment_from_io(peer_id, file_data, title=title, tags=tags)
 
     async def get_attachments_from_links(
-        self, peer_id: int, links: typing.List[str], titles: typing.Sequence[str] = ()
+        self,
+        peer_id: int,
+        links: typing.List[str],
+        titles: typing.Sequence[str] = (),
+        tags_list: typing.Sequence[str] = (),
     ) -> str:
         ready_attachments: typing.List[str] = []
-        for link, title in itertools.zip_longest(links, titles, fillvalue="Document"):
+        for link, title, tags in itertools.zip_longest(
+            links, titles, tags_list, fillvalue="Document"
+        ):
             ready_attachments.append(
-                await self.get_attachment_from_link(peer_id, link, title=title)
+                await self.get_attachment_from_link(peer_id, link, title=title, tags=tags)
             )
         return ",".join(ready_attachments)
 
@@ -100,7 +128,11 @@ class VoiceUploader(DocUploaderMixin):
         return await self._get_server(peer_id, DocsDocAttachmentType.AUDIO_MESSAGE)
 
     async def upload(
-        self, upload_url: str, file_data: BinaryIO, title: typing.Optional[str] = None
+        self,
+        upload_url: str,
+        file_data: BinaryIO,
+        title: typing.Optional[str] = None,
+        tags: typing.Optional[str] = None,
     ) -> DocsSaveResponseModel:
         if not hasattr(file_data, "name"):
             setattr(file_data, "name", "Document.ogg")
@@ -113,7 +145,7 @@ class VoiceUploader(DocUploaderMixin):
 
         self.handle_error(upload_data)
         doc_save = (
-            await self.api_context.docs.save(file=upload_data["file"], title=title)
+            await self.api_context.docs.save(file=upload_data["file"], title=title, tags=tags)
         ).response
         return doc_save
 
