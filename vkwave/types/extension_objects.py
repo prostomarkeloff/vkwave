ab61a15bdd0cddf6f7f2e725df8777813b57969d
tkgoose <62938191+tkgoose@users.noreply.github.com>
2021-01-25 12:21:03 +0900

    Update extension_objects.py

--- vkwave/types/extension_objects.py
+++ vkwave/types/extension_objects.py
@@ -5,4 +5,13 @@ class MessagesSendPeerIdsData(pydantic.BaseModel):
     peer_id: int = pydantic.Field(
         ..., description="",
     )
-
+    message_id: int = pydantic.Field(
+        None, description=""
+    )
+    conversation_message_id: int = pydantic.Field(
+        None, description=""
+    )
+    error: dict = pydantic.Field(
+        None, description=""
+    )
+ 
