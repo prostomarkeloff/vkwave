9eea32c66d3a0ff210d944eb1af22cadacfa32e4
KurimuzonAkuma <arturgayauskas@gmail.com>
2021-06-22 04:05:46 +0300

    fixed filters import

--- vkwave/bots/core/dispatching/filters/__init__.py
+++ vkwave/bots/core/dispatching/filters/__init__.py
@@ -1,18 +1,21 @@
 from .base import BaseFilter  # noqa: F401
 from .builtin import (  # noqa: F401
+    AttachmentTypeFilter,
     ChatActionFilter,
     CommandsFilter,
     EventTypeFilter,
     FromMeFilter,
     FwdMessagesFilter,
+    get_text,
+    IsAdminFilter,
     MessageArgsFilter,
     MessageFromConversationTypeFilter,
+    PayloadContainsFilter,
     PayloadFilter,
     RegexFilter,
     ReplyMessageFilter,
     TextContainsFilter,
     TextFilter,
-    get_text,
     TextStartswithFilter,
 )
 from .cast import caster as filter_caster  # noqa: F401
