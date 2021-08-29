f4f2c20aba66261168eb5820454e052abd32b6b2
Saoqq <wfmfe@yandex.ru>
2021-08-29 21:12:05 +0300

    Extend audio api
    Add getById method
    Add models
    Extend existing models

--- vkwave/api/methods/audio.py
+++ vkwave/api/methods/audio.py
@@ -5,6 +5,12 @@ from ._utils import get_params
 
 
 class Audio(Category):
+    """
+    To use audio api:
+        1) obtain token through direct auth with client_secret and client_id of app that is allowed to use audio api
+        2) set appropriate user agent for ClientSession (for instance for official vk app: VKAndroidApp/6.51)
+    """
+
     async def get(
         self,
         owner_id: typing.Optional[int] = None,
@@ -33,4 +39,24 @@ class Audio(Category):
             return raw_result
 
         result = AudioGetResponse(**raw_result)
+        return result
+
+    async def get_by_id(
+        self,
+        audios: typing.List[str],
+        return_raw_response: bool = False,
+    ) -> typing.Union[dict, AudioGetByIdResponse]:
+        """
+        :param audios: - List of audios in following format: "<owner_id>_<audio_id>"
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("getById", params)
+        if return_raw_response:
+            return raw_result
+
+        result = AudioGetByIdResponse(**raw_result)
         return result
