4788147ab9669db046639540d5b12b1d51302148
ZeN220 <79989516+ZeN220@users.noreply.github.com>
2021-11-27 23:24:08 +0300

    add to default client for api class + editing example with api (#174)
    * add to default client for api class
    
    * remove point client from api class

--- examples/work_with_api.py
+++ examples/work_with_api.py
@@ -1,17 +1,16 @@
 import asyncio
 
 from vkwave.api import API, BotSyncSingleToken, Token
-from vkwave.client import AIOHTTPClient
 
-client = AIOHTTPClient()
-api = API(tokens=BotSyncSingleToken(Token("token")), clients=client)
+
+api = API(tokens=BotSyncSingleToken(Token("token")))
 api_ctx = api.get_context()
 
 
 async def main():
-    get_user = (await api_ctx.users.get(user_ids = 1)).response[0].first_name
+    get_user = (await api_ctx.users.get(user_ids=1)).response[0].first_name
     await api_ctx.api_options.clients[0].close()
-    return get_user
+    print(get_user)
 
 
 asyncio.get_event_loop().run_until_complete(main())
