8ee44f5087ad0a24e61fad8746bc43505e13d149
ZeN220 <artaganmalsagov07@gmail.com>
2022-03-21 21:43:06 +0300

    added context manager for example with api

--- examples/work_with_api.py
+++ examples/work_with_api.py
@@ -3,14 +3,13 @@ import asyncio
 from vkwave.api import API, BotSyncSingleToken, Token
 
 
-api = API(tokens=BotSyncSingleToken(Token("token")))
-api_ctx = api.get_context()
-
-
 async def main():
-    get_user = (await api_ctx.users.get(user_ids=1)).response[0].first_name
-    await api_ctx.api_options.clients[0].close()
-    print(get_user)
+    token = BotSyncSingleToken(Token("token"))
+    async with API(tokens=token) as api:
+        api_ctx = api.get_context()
+        get_user = (await api_ctx.users.get(user_ids=1)).response[0].first_name
+        await api_ctx.api_options.clients[0].close()
+        print(get_user)
 
 
 asyncio.get_event_loop().run_until_complete(main())
